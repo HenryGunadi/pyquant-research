45adfb1712969e1933e2ab0bc10258a914f94d5a
HenryGunadi <henrywilliamgunadi09@gmail.com>
2025-05-25 15:21:23 +0700

    added some functionalities

--- classes/Strategies.py
+++ classes/Strategies.py
@@ -2,24 +2,16 @@ from abc import ABC, abstractmethod
 import pandas as pd
 from Trade import Trade
 from utils.indicators import calculate_rsi_ema
+from typing import Union
+from Account import Account
 
 class Strategy(ABC):
-    def __init__(self, trade: Trade = None):
-        self.trade: Trade = trade
-
-    @property
-    def trade(self) -> Trade:
-        return self.__trade
-    
-    @trade.setter
-    def trade(self, trade: Trade) -> None:
-        if not isinstance(trade, (Trade, None)):
-            raise TypeError("Invalid trade object type")
-
-        self.__trade = trade
+    @abstractmethod
+    def signal(self, data: pd.DataFrame | pd.Series, *args, **kwargs):
+        pass
 
     @abstractmethod
-    def execute_strategy(self, *args, **kwargs):
+    def execute_strategy(self, trade: Trade,*args, **kwargs):
         pass
 
 class RSI_Strategy(Strategy):
@@ -27,11 +19,16 @@ class RSI_Strategy(Strategy):
         super().__init__(trade)
         self.rsi_values = None
 
-    def execute_strategy(self, *args, **kwargs):
-        if kwargs.data is None:
-            raise ValueError("Missing data for using rsi strategy!")        
+    def signal(self, data, *args, **kwargs):
+        self.rsi_values = calculate_rsi_ema(data=data,
+                                            period=14)
         
-        if not isinstance(kwargs.data, (pd.DataFrame, pd.Series)):
-            raise TypeError("Invalid data type")
-
-        self.rsi_values = calculate_rsi_ema(kwargs.data)
+    def execute_strategy(self, trade: Trade, data: pd.DataFrame | pd.Series, account: Account, *args, **kwargs) -> Union[Trade, None]:
+        for index, rsi_value in enumerate(self.rsi_values):
+            # long position
+            if rsi_value <= 30:
+                pass
+                # trade = Trade(symbol=kwargs.symbol,
+                #               entry_price=data["Close"].iloc[index],
+                #               account=account,
+                #               shares=)
--- classes/Trade.py
+++ classes/Trade.py
@@ -2,6 +2,8 @@ import uuid
 from Account import Account
 from datetime import date, datetime
 from abc import ABC, abstractmethod
+from Strategies import Strategy
+from typing import Literal
 
 class ExitCondition(ABC):
     def __init__(self, expiration: datetime = None):
@@ -22,8 +24,9 @@ class StopLossExit(ExitCondition):
         self.stop_price = stop_price
 
     def should_exit(self, trade, current_price, current_time: datetime) -> bool:
-        if not self.is_expired(current_time):
-            return current_price <= self.stop_price
+        if self.is_expired(current_time):
+            return False
+        return current_price <= self.stop_price
     
 class Exit(ExitCondition):
     def __init__(self, exit_price: float, expiration: datetime = None):
@@ -31,23 +34,38 @@ class Exit(ExitCondition):
         self.exit_price = exit_price
 
     def should_exit(self, current_price, current_time: datetime) -> bool:
-        if not self.is_expired(current_time):
-            return current_price >= self.exit_price
-    
+        if self.is_expired(current_time):
+            return False
+        return current_price >= self.exit_price
+
 class Trade():
-    def __init__(self, symbol: str, entry_price: float, account: Account, shares: int, stop_loss: StopLossExit = None, exit: Exit = None):
+    def __init__(self, symbol: str, entry_time: datetime, entry_price: float, account: Account, shares: int, position_type: Literal["long", "short"], strategy: Strategy = None, stop_loss: StopLossExit = None, exit: Exit = None):
         self.__id: str = str(uuid.uuid4())
         self.symbol: str = symbol
         # assume these are non fractional shares
         self.shares: int = shares
         self.__entry_price: float = entry_price
+        self.__entry_time: datetime = entry_time
         self.stop_loss: StopLossExit = stop_loss
         self.exit: Exit = exit
         self.account: Account = account
+        self.__average_price: float = entry_price
+        self.__invested: float = shares * entry_price
+        self.strategy: Strategy = strategy
+        self.position_type = position_type
+        self.__pnl: float = 0
 
     @property
     def id(self) -> str:
         return self.__id
+    
+    @property
+    def pnl(self) -> float:
+        return self.__pnl
+    
+    @property
+    def entry_time(self) -> datetime:
+        return self.__entry_time
     
     @property
     def shares(self) -> int:
@@ -62,13 +80,38 @@ class Trade():
         return self.__entry_price
     
     @property 
-    def stop_loss(self) -> float:
+    def stop_loss(self) -> StopLossExit:
         return self.__stop_loss
     
     @property 
-    def exit(self) -> float:
+    def exit(self) -> Exit:
         return self.__exit
     
+    @property
+    def average_price(self) -> float:
+        return self.__average_price
+
+    @property
+    def strategy(self) -> Strategy:
+        return self.__strategy
+    
+    @property
+    def invested(self) -> float:
+        return self.__invested
+    
+    @property
+    def pnl_percentage(self) -> float:
+        if self.__invested == 0:
+            return 0
+        return (self.__pnl / self.__invested) * 100
+    
+    @strategy.setter
+    def strategy(self, strategy: Strategy) -> None:
+        if not isinstance(strategy, (Strategy, None)):
+            raise TypeError("Invalid strategy object type")
+
+        self.__strategy = strategy
+
     @stop_loss.setter
     def stop_loss(self, stop_loss: StopLossExit) -> None:
         if not isinstance(stop_loss, (StopLossExit, None)):
@@ -97,9 +140,17 @@ class Trade():
 
         self.__shares = shares
 
-    def increase_share(self, shares: int) -> None:
+    def update_pnl(self, current_price: float):
+        if self.position_type == "long":
+            self.__pnl = (current_price - self.average_price) * self.shares
+        else:
+            self.__pnl = (self.average_price - current_price) * self.shares
+
+    def increase_share(self, shares: int, price: float) -> None:
         if shares <= 0:
             raise ValueError("Shares cannot be equal or less than zero")
-    
-        self.__account.can_execute_trade() 
+        self.__account.can_execute_trade()
+        total_cost = self.__average_price * self.__shares + price * shares
         self.__shares += shares
+        self.__average_price = total_cost / self.__shares
+        self.__invested = self.__average_price * self.__shares
